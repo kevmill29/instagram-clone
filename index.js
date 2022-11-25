a3683996b605da218a48c7afd204477c33cc790e
kevmill29 <96067818+kevmill29@users.noreply.github.com>
2022-11-24 23:28:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,116 +1,77 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+import { posts } from './data.js'
+
+
+// Incrementing and Decrementing Like Count
+
+document.addEventListener('click', function(e){
+    if(e.target.dataset.heart){
+       handleLikeClick(e.target.dataset.heart) 
     }
-]
+})
 
-const tlDOM = document.getElementById("tl-div")
 
-function renderPosts(){
-  let postTL = ""
-  for (let i=0; i < posts.length; i++){
-    postTL += `
+function handleLikeClick(postId){ 
+    const targetPostObj = posts.filter(function(post){
+        return post.username === postId
+    })[0]
+    
+    if (targetPostObj.isLiked){
+        targetPostObj.likes--
+        
+    } else{
+        targetPostObj.likes++
+        
+    }
+    targetPostObj.isLiked = !targetPostObj.isLiked
+    render()
+   
+}
+
+// Feed Rendering Posts
+function getHtmlFeed(){ 
+ let igPosts = ``
+
+    posts.forEach(function(post){
+       igPosts += `
 <section>
   <div class="post-div">
     <div class="user-loc">
-      <img class="avatar" src="${posts[i].avatar}"/>
+      <img class="avatar" src="${post.avatar}"/>
         <p class="user-info">
-          <span><strong>${posts[i].name}</strong></span><br>
-          <span>${posts[i].location}</span>
+          <span><strong>${post.name}</strong></span><br>
+          <span>${post.location}</span>
         </p>
     </div>
     <div "img-container">  
-      <img class="post-img" id="${posts[i].username}-post" src="${posts[i].post}">
+      <img class="post-img"  src="${post.post}">
     </div>
     <div class="button-sect">
-      <img id="${posts[i].username}-likebtn" src="images/icon-heart.png" alt="like this post" class="icon">
-      <img src="images/icon-comment.png" alt="comment on this post" class="icon">
-      <img src="images/icon-dm.png" alt="share this post" class="icon">
+      <img id="likebtn" 
+      src="images/icon-heart.png" 
+      alt="like this post" 
+      class="icon" 
+      data-heart=${post.username}>
+      <img src="images/icon-comment.png" 
+      alt="comment on this post" 
+      class="icon">
+      <img src="images/icon-dm.png" 
+      alt="share this post" 
+      class="icon">
     </div>
     <div class="post-btm">
-      <p id="${posts[i].username}-likes"><strong>${posts[i].likes} likes</strong><p><br>
-      <p><strong>${posts[i].username}</strong> ${posts[i].comment}<p>
+      <p -likes" data-heart="${post.username}"><strong>${post.likes} likes</strong><p><br>
+      <p><strong>${post.username}</strong> ${post.comment}<p>
   </div>
-</section>
-       `
-  }
-  tlDOM.innerHTML = postTL
-}
+</section>`
+    })
+ return igPosts
+}   
 
-renderPosts()
-
-//Increasing/decreasing likes with both 
-//post double click and heart icon click
-
-const postIdsDOM = []
-const likeBtnDOM = []
-const likesIdsDOM = []
-const likesCheck = []
+function render(){
+ document.getElementById("posts").innerHTML = getHtmlFeed()
+ } 
+render()
 
 
 
-
-
-for (let i = 0; i < posts.length; i++) {
-    postIdsDOM.push(document.getElementById(`${posts[i].username}-post`))
-    likesIdsDOM.push(document.getElementById(`${posts[i].username}-likes`))
-    likeBtnDOM.push(document.getElementById(`${posts[i].username}-likebtn`))
-    likesCheck.push(posts[i].likes)
-}
-
-postIdsDOM[0].addEventListener("dblclick", function(){
-    increaseLikes(0)
-})
-
-postIdsDOM[1].addEventListener("dblclick", function(){
-    increaseLikes(1)  
-})
-
-postIdsDOM[2].addEventListener("dblclick", function(){
-    increaseLikes(2)
-})
-
-likeBtnDOM[0].addEventListener("click", function(){
-    increaseLikes(0)
-})
-
-likeBtnDOM[1].addEventListener("click", function(){
-    increaseLikes(1)  
-})
-
-likeBtnDOM[2].addEventListener("click", function(){
-    increaseLikes(2)
-})
-function increaseLikes(i) {
-    if (posts[i].likes === likesCheck[i]){
-        posts[i].likes += 1
-        likesIdsDOM[i].innerHTML = `<strong>${posts[i].likes} likes</strong>`
-    } else if (posts[i].likes === likesCheck[i] + 1) {
-        posts[i].likes -= 1
-        likesIdsDOM[i].innerHTML = `<strong>${posts[i].likes} likes</strong>`
-    }
-}
+   
